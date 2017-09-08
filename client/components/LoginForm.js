77ca8d8a54ad5232655d0fa20355d3b92d792f28
Raul Gutierrez <rdgutierrez@gmail.com>
2017-09-08 13:01:03 -0700

    added login functionality

---
+++ client/components/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import AuthForm from "./AuthForm";
+import mutation from "../mutations/Login";
+import { graphql } from "react-apollo";
+
+class LoginForm extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(LoginForm);
